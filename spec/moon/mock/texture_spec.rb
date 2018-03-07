6ad29a588eb9b0a5999b942194f422fe2778da41
Corey Powell <mistdragon100@gmail.com>
2018-03-07 00:08:55 -0500

    Fixed texture_spec warning

--- spec/moon/mock/texture_spec.rb
+++ spec/moon/mock/texture_spec.rb
@@ -8,7 +8,7 @@ describe Moon::Texture do
     end
 
     it 'fails to create a texture if the file does not exist' do
-      expect { Moon::Texture.new(fixture_pathname('fixtures/resources/test_derp.png')) }.to raise_error
+      expect { Moon::Texture.new(fixture_pathname('fixtures/resources/test_derp.png')) }.to raise_error(ScriptError)
     end
   end
 end
